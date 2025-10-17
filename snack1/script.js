ce5aed49628848d6bed22caed6213e3994c348bd
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-17 14:21:11 +0200

    snack1: create array with two objects

--- snack1/script.js
+++ snack1/script.js
@@ -6,9 +6,18 @@ Stampare a schermo la bici con peso minore.
 */
 
 //Creare un array di oggetti
-// - const bicycles = [{}, {}..]
-//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-// - {nome: "nome", peso: "peso"}
+const bicycles = [
+  //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+  {
+    name: "BMX",
+    weight: 20
+  },
+  
+  {
+    name: "MB",
+    weight: 30
+  }
+]
 
 //Stampare a schermo la bici con peso minore
 // - Scorro nell'array di oggetti

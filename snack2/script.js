411da89a25da07770d34392a310a65dd724a307f
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-17 17:10:48 +0200

    snack2: create objetcs array

--- snack2/script.js
+++ snack2/script.js
@@ -8,8 +8,30 @@ Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subit
 */
 
 // Creare un array di oggetti di squadre di calcio.
-// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+const teams = [
+  // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+  // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+  {
+    name: "Milan",
+    score: 0,
+    fouls: 0
+  },
+  {
+    name: "Inter",
+    score: 0,
+    fouls: 0
+  },
+  {
+    name: "Juventus",
+    score: 0,
+    fouls: 0
+  },
+  {
+    name: "Napoli",
+    score: 0,
+    fouls: 0
+  }
+];
 
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // - Math.random()

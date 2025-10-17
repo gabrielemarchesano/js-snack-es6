d29fcc12f1e61582370833635320f37b97b4942f
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-17 16:11:00 +0200

    snack1: add for loop to loop the array, add if condition to check which objects has the smallest weight property

--- snack1/script.js
+++ snack1/script.js
@@ -10,19 +10,43 @@ const bicycles = [
   //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
   {
     name: "BMX",
-    weight: 20
+    weight: 40
   },
-  
   {
     name: "MB",
     weight: 30
+  },
+  {
+    name: "City Bike",
+    weight: 20
+  },
+  {
+    name: "Road Bike",
+    weight: 10
+  },
+  {
+    name: "Electric Bike",
+    weight: 50
   }
 ]
+//Stampo in console l'array per un controllo
+console.log(bicycles);
 
-//Stampare a schermo la bici con peso minore
+
+let smallerIndex = 0;
+let firstBicycle = bicycles[0];
+
 // - Scorro nell'array di oggetti
-// - Controllo la proprietà peso dell'oggetto in posizione i
-  // - Se è più piccola
-    // - Stampo la bici selezionata
-  // - Altrimenti
-    // - Stampo l'altra
+for (let i = 0; i < bicycles.length; i++) {
+  // - Controllo la proprietà peso dell'oggetto in posizione i
+  if(bicycles[i].weight <= firstBicycle.weight){
+    //leggera = bicycles[i];
+    // - Se è più piccola o uguale
+    // - salvo indice bicycles[i] in smallerIndex
+    smallerIndex = i;
+  } 
+}
+
+console.log(smallerIndex);
+// - Stampo la bici selezionata
+console.log(bicycles[smallerIndex]);
